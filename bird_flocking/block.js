12737c2245673bb6f68efec9f1e19de255e65712
gaiborjosue <gaiborjimenezjosue@gmail.com>
2023-10-23 22:36:03 -0400

    fix(ecma): Fixed the app to work with ecma6

--- bird_flocking/block.js
+++ bird_flocking/block.js
@@ -1,7 +1,8 @@
+import * as THREE from 'three';
 var Block = function(x, y, z) {
 
 
-	this.geometry     = new THREE.SphereBufferGeometry( 5, 10, 10 );
+	this.geometry     = new THREE.SphereGeometry( 5, 10, 10 );
 	this.material     = new THREE.MeshBasicMaterial({ color: 0xffffff });
 	this.mesh         = new THREE.Mesh(this.geometry, this.material);
 	this.mesh.position.set(x, y, z);
@@ -12,3 +13,5 @@ Block.prototype.show = function(scene) {
   scene.add( this.mesh );
   
 };
+
+export default Block;
